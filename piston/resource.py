9196a96b7ecbbeaa03e890ca0957a547a726a054
linares <alexandre.linares@yahoo.com>
2014-03-12 23:13:57 -0700

    latest resource

--- piston/resource.py
+++ piston/resource.py
@@ -1,5 +1,6 @@
-import sys, inspect, re
+import sys, inspect
 
+import django
 from django.http import (HttpResponse, Http404, HttpResponseNotAllowed,
     HttpResponseForbidden, HttpResponseServerError)
 from django.views.debug import ExceptionReporter
@@ -9,11 +10,16 @@ from django.core.mail import send_mail, EmailMessage
 from django.db.models.query import QuerySet
 from django.http import Http404
 
+try:
+    import mimeparse
+except ImportError:
+    mimeparse = None
+
 from emitters import Emitter
 from handler import typemapper
 from doc import HandlerMethod
 from authentication import NoAuthentication
-from utils import coerce_put_post, FormValidationError, HttpStatusCode, BadRangeException
+from utils import coerce_put_post, FormValidationError, HttpStatusCode
 from utils import rc, format_error, translate_mime, MimerDataException
 
 CHALLENGE = object()
@@ -28,8 +34,6 @@ class Resource(object):
     """
     callmap = { 'GET': 'read', 'POST': 'create',
                 'PUT': 'update', 'DELETE': 'delete' }
-
-    range_re = re.compile("^items=(\d*)-(\d*)$")
 
     def __init__(self, handler, authentication=None):
         if not callable(handler):
@@ -49,28 +53,35 @@ class Resource(object):
         self.email_errors = getattr(settings, 'PISTON_EMAIL_ERRORS', True)
         self.display_errors = getattr(settings, 'PISTON_DISPLAY_ERRORS', True)
         self.stream = getattr(settings, 'PISTON_STREAM_OUTPUT', False)
-        
-        # Paging
-        paging_params = getattr(settings, 'PISTON_PAGINATION_PARAMS', ('offset', 'limit'))
-        self.paging_offset = paging_params[0]
-        self.paging_limit = paging_params[1]
+        # Emitter selection
+        self.strict_accept = getattr(settings, 'PISTON_STRICT_ACCEPT_HANDLING',
+                                     False)
+        self.default_emitter = getattr(settings, 'PISTON_DEFAULT_EMITTER',
+                                       'json')
 
     def determine_emitter(self, request, *args, **kwargs):
         """
         Function for determening which emitter to use
         for output. It lives here so you can easily subclass
         `Resource` in order to change how emission is detected.
-
-        You could also check for the `Accept` HTTP header here,
-        since that pretty much makes sense. Refer to `Mimer` for
-        that as well.
         """
-        em = kwargs.pop('emitter_format', None)
-
-        if not em:
-            em = request.GET.get('format', 'json')
-
-        return em
+        try:
+            return kwargs['emitter_format']
+        except KeyError:
+            pass
+        if 'format' in request.GET:
+            return request.GET.get('format')
+        if mimeparse and 'HTTP_ACCEPT' in request.META:
+            supported_mime_types = set()
+            emitter_map = {}
+            for name, (klass, content_type) in Emitter.EMITTERS.items():
+                content_type_without_encoding = content_type.split(';')[0]
+                supported_mime_types.add(content_type_without_encoding)
+                emitter_map[content_type_without_encoding] = name
+            preferred_content_type = mimeparse.best_match(
+                list(supported_mime_types),
+                request.META['HTTP_ACCEPT'])
+            return emitter_map.get(preferred_content_type, None)
 
     def form_validation_response(self, e):
         """
@@ -79,7 +90,7 @@ class Resource(object):
         `Resource` subclass.
         """
         resp = rc.BAD_REQUEST
-        resp.write(' '+str(e.form.errors))
+        resp.write(u' '+unicode(e.form.errors))
         return resp
 
     @property
@@ -157,8 +168,14 @@ class Resource(object):
         if not meth:
             raise Http404
 
-        # Support emitter both through (?P<emitter_format>) and ?format=emitter.
+        # Support emitter through (?P<emitter_format>) and ?format=emitter
+        # and lastly Accept: header processing
         em_format = self.determine_emitter(request, *args, **kwargs)
+        if not em_format:
+            request_has_accept = 'HTTP_ACCEPT' in request.META
+            if request_has_accept and self.strict_accept:
+                return rc.NOT_ACCEPTABLE
+            em_format = self.default_emitter
 
         kwargs.pop('emitter_format', None)
 
@@ -188,195 +205,14 @@ class Resource(object):
         # If we're looking at a response object which contains non-string
         # content, then assume we should use the emitter to format that 
         # content
-	msg = ''
-
-        if isinstance(result, HttpResponse) and not result._is_string:
+        if self._use_emitter(result):
             status_code = result.status_code
-            # Note: We can't use result.content here because that method attempts
-            # to convert the content into a string which we don't want. 
-            # when _is_string is False _container is the raw data
+            # Note: We can't use result.content here because that
+            # method attempts to convert the content into a string
+            # which we don't want.  when
+            # _is_string/_base_content_is_iter is False _container is
+            # the raw data
             result = result._container
-            if sig:
-                msg += 'Signature should be: %s' % sig
-     
-        srl = emitter(result, typemapper, handler, fields, anonymous)
-
-        try:
-            """
-            Decide whether or not we want a generator here,
-            or we just want to buffer up the entire result
-            before sending it to the client. Won't matter for
-            smaller datasets, but larger will have an impact.
-            """
-            if self.stream: stream = srl.stream_render(request)
-            else: stream = srl.render(request)
-
-            if not isinstance(stream, HttpResponse):
-                resp = HttpResponse(stream, mimetype=ct, status=status_code)
-            else:
-                resp = stream
-
-            if self.display_errors:
-                msg += '\n\nException was: %s' % str(e)
-            resp.streaming = self.stream
-            result.content = format_error(msg)
-        except Http404:
-            return rc.NOT_FOUND
-        except HttpStatusCode, e:
-            return e.response
-        except Exception, e:
-            """
-            On errors (like code errors), we'd like to be able to
-            give crash reports to both admins and also the calling
-            user. There's two setting parameters for this:
-
-            Parameters::
-             - `PISTON_EMAIL_ERRORS`: Will send a Django formatted
-               error email to people in `settings.ADMINS`.
-             - `PISTON_DISPLAY_ERRORS`: Will return a simple traceback
-               to the caller, so he can tell you what error they got.
-
-            If `PISTON_DISPLAY_ERRORS` is not enabled, the caller will
-            receive a basic "500 Internal Server Error" message.
-            """
-            exc_type, exc_value, tb = sys.exc_info()
-            rep = ExceptionReporter(request, exc_type, exc_value, tb.tb_next)
-            if self.email_errors:
-                self.email_exception(rep)
-            if self.display_errors:
-                return HttpResponseServerError(
-                    format_error('\n'.join(rep.format_exception())))
-            else:
-                raise
-
-        content_range = None
-        if isinstance(result, QuerySet):
-            """
-            Limit results based on requested items. This is a based on
-            HTTP 1.1 Partial GET, RFC 2616 sec 14.35, but is intended to
-            operate on the record level rather than the byte level.  We
-            will still respond with code 206 and a range header.
-            """
-
-            request_range = None
-            
-            if 'HTTP_RANGE' in request.META:
-                """
-                Parse the reange request header. HTTP proper expects Range, 
-                but since we are deviating from the bytes nature, we will use 
-                a non-standard syntax. This is expected to be of the format:
-
-                    "items" "=" start "-" end
-
-                E.g.,
-
-                    Range: items=7-45
-
-                """
-                h = request.META['HTTP_RANGE'].strip()
-                if h.startswith('items='):
-                    m = self.range_re.match(h)
-                    if m:
-                        s, e = None, None
-                        if m.group(1) != '': s = int(m.group(1))
-                        if m.group(2) != '': e = int(m.group(2))
-                        request_range = (s, e)
-                    else:
-                        resp = rc.BAD_REQUEST
-                        resp.write(' malformed range header')
-                        return resp 
-
-            elif self.paging_offset in request.GET and self.paging_limit in request.GET:
-                """
-                Alternatively, parse the query string for paging parameters. Here,
-                we ask for an offset and a limit, so we need to convert this to a 
-                fixed start and end to accomodate the slicing. Both parameters must 
-                be specified, but either may be left empty, exclusively, to produce
-                the following behaviors:
-
-                    * ?offset=n&limit= -> tail of list, beginning at item n
-                    * ?offset=&limit=n -> trailing n items of list
-
-                """
-                try: offset = int(request.GET[self.paging_offset])
-                except (ValueError, TypeError): offset = None
-
-                try: limit = int(request.GET[self.paging_limit])
-                except (ValueError, TypeError): limit = None
-
-                if offset != None and limit != None: request_range = (offset, offset + limit - 1)
-                elif offset != None: request_range = (offset, None)
-                elif limit != None: request_range = (None, limit)
-
-            if request_range:
-                def get_range(start, end, total):
-                    """
-                    Normalizes the range for queryset slicing and response
-                    header generation.  Checks that constraints defined
-                    by the RFC hold, or raises exceptions.
-                    """
-                    
-                    if start != None and start < 0: raise BadRangeException('negative ranges not allowed')
-                    if end != None and end < 0: raise BadRangeException('negative ranges not allowed')
-                    
-                    range_start = start
-                    range_end = end
-                    last = total - 1
-
-                    if range_start != None and range_end != None:
-                        """
-                        Basic, well-formed range.  Make sure that requested range is
-                        between 0 and last inclusive, and that start <= end. 
-                        """
-                        if range_start > last: raise BadRangeException("start gt last")
-                        if range_start > range_end: raise BadRangeException("start lt end")
-                        if range_end > last: range_end = last;
-
-                    elif range_start != None:
-                        """
-                        Requsting range_start through last.  Make sure range_start is
-                        valid and set range_end to last.
-                        """
-                        if range_start > last: raise BadRangeException("start gt last")
-                        range_end = last
-
-                    elif range_end != None:
-                        """
-                        Requesting range_end items from the tail of the result set.
-                        If range_end > last, the entire resultset is returned.  otherwise
-                        range_start must be last - range_end.
-                        """
-                        if range_end > last:
-                            range_start = 0
-                            range_end = last
-                        else:
-                            range_start = last - range_end + 1
-                            range_end = last
-
-                    else:
-                        raise BadRangeException("no start or end")
-                  
-                    assert range_start != None
-                    assert range_end != None 
-                    return (range_start, range_end)
-
-
-                try:
-                    total = result.count()
-                    start, end = get_range(request_range[0], request_range[1], total)
-                    result = result[start:end + 1]
-                    content_range = "items %i-%i/%i" % (start, end, total)
-                except BadRangeException, e:
-                    resp = rc.BAD_RANGE
-                    resp.write("\n%s" % e.value)
-                    return resp
-
-
-        emitter, ct = Emitter.get(em_format)
-        fields = handler.fields
-        if hasattr(handler, 'list_fields') and (
-                isinstance(result, list) or isinstance(result, QuerySet)):
-            fields = handler.list_fields
 
         srl = emitter(result, typemapper, handler, fields, anonymous)
 
@@ -396,14 +232,20 @@ class Resource(object):
                 resp = stream
 
             resp.streaming = self.stream
-
-            if content_range:
-                resp.status_code = 206
-                resp['Content-Range'] = content_range
 
             return resp
         except HttpStatusCode, e:
             return e.response
+
+    @staticmethod
+    def _use_emitter(result):
+        """True iff result is a HttpResponse and contains non-string content."""
+        if not isinstance(result, HttpResponse):
+            return False
+        elif django.VERSION >= (1, 4):
+            return result._base_content_is_iter
+        else:
+            return not result._is_string
 
     @staticmethod
     def cleanup_request(request):
